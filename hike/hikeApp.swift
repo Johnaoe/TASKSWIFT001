4496641e7496b0163a58d3abcd7009210c6f42e3
Jonas Narbutas <jonas.narbutas@NB-MAC-JONASN.betgames.lan>
2023-10-11 11:53:20 +0300

    initial

---
+++ hike/hikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  hikeApp.swift
+//  hike
+//
+//  Created by Jonas Narbutas on 2023-10-11.
+//
+
+import SwiftUI
+
+@main
+struct hikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
